7444093823023beef0bda10529b22f7263a1d5fe
wenyang <yang.t.skywalker@gmail.com>
2024-02-22 21:28:35 +0800

    add more titles

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+TITLE_NUM = 3
+
 const titleElement = new TypeIt("#can-not-stop", {
-  speed: 200,
+  speed: 100,
 });
 
-titleElement.type("Helo!")
-  .pause(500)
-  .move(-2)
-  .type("l", {delay: 400})
-  .move(2)
-  .go();
+randomNumber = getRandomInt(TITLE_NUM);
+
+if (randomNumber === 0) {
+  titleElement.type("Helo!")
+    .pause(500)
+    .move(-2)
+    .type("l", {delay: 400})
+    .move(2)
+    .type("😆")
+    .go();
+} else if (randomNumber === 1) {
+  titleElement.type("More power!")
+    .pause(500)
+    .move(-6)
+    .type("compute ", {delay: 400})
+    .move(6)
+    .type("👀")
+    .go();
+} else if (randomNumber === 2) {
+  titleElement.type("Something's happening!🤨")
+    .pause(500)
+    .go();
+}
 
 // add last modified date of web page
 const repoName = "notoookay/notoookay.github.io";
